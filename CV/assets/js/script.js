5e6f8e788df8305ca6929f4bebb126121ec15478
Wesley LEVASSEUR <mailpro.levasseur@icloud.com>
2022-07-28 23:49:00 +0200

    Pre-Release CV
    - Feature : FR/EN choose

--- CV/assets/js/script.js
+++ CV/assets/js/script.js
@@ -1,10 +1,17 @@
 noclean.addEventListener('click', (_) => {
     main.setAttribute('clean', 'false')
+    download.href = 'assets/download/cv.pdf'
 })
 
 clean.addEventListener('click', (_) => {
     main.setAttribute('clean', 'true')
+    download.href = 'assets/download/cv-clean.pdf'
 })
+
+if (urlParams['theme'] === 'clean') {
+    main.setAttribute('clean', 'true')
+    download.href = 'assets/download/cv-clean.pdf'
+}
 
 setTimeout(() => {
     fetch('assets/json/data.json')
@@ -14,23 +21,39 @@ setTimeout(() => {
             headerSubtitle1.textContent = json['header-subtitle-1']
             headerSubtitle2.textContent = json['header-subtitle-2'].replace('{AGE}', age)
 
+            // noinspection JSValidateTypes
             temps.textContent = json['temps']
 
             columnLeftDescription.textContent = json['column-left-description']
 
+            // noinspection JSCheckFunctionSignatures
             Array.from(json['skills']).forEach(str => {
                 let _e = document.createElement('li')
-                _e.innerText = str
+                // noinspection JSValidateTypes
+                _e.textContent = str
                 skills.appendChild(_e)
             })
 
+            // noinspection JSCheckFunctionSignatures
+            Array.from(json['column-experiences']).forEach(map => {
+                columnRightExperiences.appendChild(createElement(map['title'], map['subtitle'], map['content']))
+            })
+
+            // noinspection JSCheckFunctionSignatures
+            Array.from(json['column-formations']).forEach(map => {
+                columnRightFormations.appendChild(createElement(map['title'], map['subtitle'], map['content']))
+            })
+
+            // noinspection JSCheckFunctionSignatures
             Array.from(json['techniques']).forEach(str => {
                 let _e = document.createElement('img')
                 _e.src = `assets/img/techniques/${str}.png`
+                // noinspection JSValidateTypes
                 _e.alt = str
                 techniques.appendChild(_e)
             })
 
+            // noinspection JSCheckFunctionSignatures
             hobbies.textContent = Array.from(json['hobbies']).join(', ') + '.'
 
             contactMail.textContent = json['contact']['mail']
@@ -40,5 +63,5 @@ setTimeout(() => {
             contactAddress.textContent = json['contact']['address']
 
             ldsRoller.setAttribute('loading', 'false')
-        });
+        })
 }, 1000)
--- CV/assets/js/utils.js
+++ CV/assets/js/utils.js
@@ -3,25 +3,74 @@ const main = document.querySelector('main')
 const noclean = document.querySelector('#noclean')
 const clean = document.querySelector('#clean')
 
-const ldsRoller = document.querySelector('.lds-roller');
+const ldsRoller = document.querySelector('.lds-roller')
+const download = document.querySelector('#download')
 
-const headerSubtitle1 = document.querySelector('#headerSubtitle1');
-const headerSubtitle2 = document.querySelector('#headerSubtitle2');
+const headerSubtitle1 = document.querySelector('#headerSubtitle1')
+const headerSubtitle2 = document.querySelector('#headerSubtitle2')
 
-const temps = document.querySelector('.temps');
+const temps = document.querySelector('.temps')
 
-const columnLeftDescription = document.querySelector('.column-left > .description');
+const columnLeftDescription = document.querySelector('.column-left > .description')
+const columnRightExperiences = document.querySelector('#experiences')
+const columnRightFormations = document.querySelector('#formations')
 
 const skills = document.querySelector('#skills')
 const techniques = document.querySelector('#techniques')
 const hobbies = document.querySelector('#hobbies')
 
-const contactMail = document.querySelector('#contact #mail');
-const contactPhone = document.querySelector('#contact #phone');
-const contactGithub = document.querySelector('#contact #github');
-const contactLinkedin = document.querySelector('#contact #linkedin');
-const contactAddress = document.querySelector('#contact #address');
+const contactMail = document.querySelector('#contact #mail')
+const contactPhone = document.querySelector('#contact #phone')
+const contactGithub = document.querySelector('#contact #github')
+const contactLinkedin = document.querySelector('#contact #linkedin')
+const contactAddress = document.querySelector('#contact #address')
 
-const birthdays = new Date('06/08/2003');
-const nowDate = new Date();
-const age = (nowDate.getFullYear() - birthdays.getFullYear()) - (nowDate.getMonth() < nowDate.getMonth() ? 1 : nowDate.getDate() < birthdays.getDate() ? 1 : 0);
+const birthdays = new Date('06/08/2003')
+const nowDate = new Date()
+const age = (nowDate.getFullYear() - birthdays.getFullYear()) - (nowDate.getMonth() < nowDate.getMonth() ? 1 : nowDate.getDate() < birthdays.getDate() ? 1 : 0)
+
+const urlSearchParams = new URLSearchParams(window.location.search)
+const urlParams = Object.fromEntries(urlSearchParams.entries())
+
+const createElement = (title, subtitle, contents) => {
+    let _element = document.createElement('div')
+    _element.setAttribute('class', 'element')
+
+
+    let _headElement = document.createElement('div')
+    _headElement.setAttribute('class', 'head-element')
+
+    let _title = document.createElement('h1')
+    _title.setAttribute('class', 'title')
+    _title.textContent = title
+
+    let _subtitle = document.createElement('h2')
+    _subtitle.setAttribute('class', 'subtitle')
+    _subtitle.textContent = subtitle
+
+    _headElement.appendChild(_title)
+    _headElement.appendChild(_subtitle)
+
+    _element.appendChild(_headElement)
+
+    if (contents.length > 0) {
+
+        let _content = document.createElement('div')
+        _content.setAttribute('class', 'content')
+
+        let _ul = document.createElement('ul')
+        _ul.setAttribute('no-style', 'false')
+
+        contents.forEach(str => {
+            let _li = document.createElement('li')
+            _li.textContent = str
+
+            _ul.appendChild(_li)
+        })
+
+        _content.appendChild(_ul)
+        _element.appendChild(_content)
+    }
+
+    return _element
+}
