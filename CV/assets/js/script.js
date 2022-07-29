5fe526f3e62d20fed1684071fcea3ab9796f03b6
Wesley LEVASSEUR <mailpro.levasseur@icloud.com>
2022-07-29 03:11:08 +0200

    Lot of Update for Pre-Release

--- CV/assets/js/script.js
+++ CV/assets/js/script.js
@@ -13,55 +13,53 @@ if (urlParams['theme'] === 'clean') {
     download.href = 'assets/download/cv-clean.pdf'
 }
 
-setTimeout(() => {
-    fetch('assets/json/data.json')
-        .then(response => response.json())
-        .then(json => {
+fetch('assets/json/data.json')
+    .then(response => response.json())
+    .then(json => {
 
-            headerSubtitle1.textContent = json['header-subtitle-1']
-            headerSubtitle2.textContent = json['header-subtitle-2'].replace('{AGE}', age)
+        headerSubtitle1.textContent = json['header-subtitle-1']
+        headerSubtitle2.textContent = json['header-subtitle-2'].replace('{AGE}', age)
 
+        // noinspection JSValidateTypes
+        temps.textContent = json['temps']
+
+        columnLeftDescription.textContent = json['column-left-description']
+
+        // noinspection JSCheckFunctionSignatures
+        Array.from(json['skills']).forEach(str => {
+            let _e = document.createElement('li')
             // noinspection JSValidateTypes
-            temps.textContent = json['temps']
-
-            columnLeftDescription.textContent = json['column-left-description']
-
-            // noinspection JSCheckFunctionSignatures
-            Array.from(json['skills']).forEach(str => {
-                let _e = document.createElement('li')
-                // noinspection JSValidateTypes
-                _e.textContent = str
-                skills.appendChild(_e)
-            })
-
-            // noinspection JSCheckFunctionSignatures
-            Array.from(json['column-experiences']).forEach(map => {
-                columnRightExperiences.appendChild(createElement(map['title'], map['subtitle'], map['content']))
-            })
-
-            // noinspection JSCheckFunctionSignatures
-            Array.from(json['column-formations']).forEach(map => {
-                columnRightFormations.appendChild(createElement(map['title'], map['subtitle'], map['content']))
-            })
-
-            // noinspection JSCheckFunctionSignatures
-            Array.from(json['techniques']).forEach(str => {
-                let _e = document.createElement('img')
-                _e.src = `assets/img/techniques/${str}.png`
-                // noinspection JSValidateTypes
-                _e.alt = str
-                techniques.appendChild(_e)
-            })
-
-            // noinspection JSCheckFunctionSignatures
-            hobbies.textContent = Array.from(json['hobbies']).join(', ') + '.'
-
-            contactMail.textContent = json['contact']['mail']
-            contactPhone.textContent = json['contact']['phone']
-            contactGithub.textContent = json['contact']['github']
-            contactLinkedin.textContent = json['contact']['linkedin']
-            contactAddress.textContent = json['contact']['address']
-
-            ldsRoller.setAttribute('loading', 'false')
+            _e.textContent = str
+            skills.appendChild(_e)
         })
-}, 1000)
+
+        // noinspection JSCheckFunctionSignatures
+        Array.from(json['column-experiences']).forEach(map => {
+            columnRightExperiences.appendChild(createElement(map['title'], map['subtitle'], map['content']))
+        })
+
+        // noinspection JSCheckFunctionSignatures
+        Array.from(json['column-formations']).forEach(map => {
+            columnRightFormations.appendChild(createElement(map['title'], map['subtitle'], map['content']))
+        })
+
+        // noinspection JSCheckFunctionSignatures
+        Array.from(json['techniques']).forEach(str => {
+            let _e = document.createElement('img')
+            _e.src = `assets/img/techniques/${str}.png`
+            // noinspection JSValidateTypes
+            _e.alt = str
+            techniques.appendChild(_e)
+        })
+
+        // noinspection JSCheckFunctionSignatures
+        hobbies.textContent = Array.from(json['hobbies']).join(', ') + '.'
+
+        contactMail.textContent = json['contact']['mail']
+        contactPhone.textContent = json['contact']['phone']
+        contactGithub.textContent = json['contact']['github']
+        contactLinkedin.textContent = json['contact']['linkedin']
+        contactAddress.textContent = json['contact']['address']
+
+        ldsRoller.setAttribute('loading', 'false')
+    })
